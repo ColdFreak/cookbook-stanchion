ce9a20becda5b7cbd666b2715f344cc4fe8abe49
Wang Zhijun <so@idcf.jp>
2016-06-20 16:47:04 +0900

    Fix stanchion recipe

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 default['stanchion']['major_number'] = "2"
 default['stanchion']['minor_number'] = "1"
-default['stanchion']['increment_number'] = "1"
+default['stanchion']['incremental'] = "1"
 default['stanchion']['build'] = "1"
 
 default['riak-cs']['admin_key'] = "admin-key"
